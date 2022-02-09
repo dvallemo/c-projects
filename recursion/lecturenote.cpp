f87bcb5ab246d115ddc008df7853a8f24c285735
David Valle <52580367+dvallemo@users.noreply.github.com>
2022-02-09 17:05:08 -0500

    Update lecturenote.cpp

--- recursion/lecturenote.cpp
+++ recursion/lecturenote.cpp
@@ -1,1 +1,25 @@
+//recursion works backwards does else first 
+//calculates factorial without for loop
+int fact(int N)
+{
+    //Answer to the simplest instance of the problem 
+    //THe base case or anchor value
+    if (N==1)
+    return 1;
+    //reducing the oridinal instance towards the base case
+    //exploiting a pattern that is inherent in the problem 
+    else{
+        reuturn N*fact(N-1)
+    }
+}
 
+
+int main() 
+{
+    int N;
+    cout << "Give me a number: "<< endl;
+    cin >> N;
+
+    cout << "The factorial is: " << factorial(N) <<endl;
+    return 0;
+}
