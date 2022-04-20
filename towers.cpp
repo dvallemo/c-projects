87edf1fb2ba6bb55c7cefb1c4f205eb7b0af7341
David Valle <52580367+dvallemo@users.noreply.github.com>
2022-04-19 23:25:45 -0400

    Create towers.cpp

---
+++ towers.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+void hanoi(int N, char S, char I, char D);
+{
+    //Base case 
+    if (N==1)
+    {
+        cout << "Move " << N << " from " << S << " ---->"<< D endl;     
+    }
+    //Drive N towards base case
+    else {
+        hanoi(N-1, S, D, I);
+        cout << "Move " << N << " from " << S << " ---->"<< D endl; 
+        hanoi (N-1, I, S, D);
+    }
+}
+
+int main() 
+{
+    int N; 
+    char S = 'S', I ='I', D='D';
+
+    cout << "Give me the # of discs: " << endl;
+    cin >> N;
+    hanoi ( N, S, I, D);
+    return 0;
+}
